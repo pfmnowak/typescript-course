e386e573f25ac535b39a74889cb0250eeefaeb19
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2023-03-08 19:54:08 +0100

    Create a file for basic Classes with TypeScript examples

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,82 @@
-// Code goes here!
+class Department {
+  private readonly id: string;
+  private name: string;
+  protected employees: string[] = [];
+
+  constructor(id: string, n: string) {
+    this.id = id;
+    this.name = n;
+  }
+
+  describe() {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+}
+
+const it = new ITDepartment("d2", ["Jack"]);
+console.log(it);
+
+const accounting = new AccountingDepartment("d3", []);
+accounting.addReport("Something went wrong...");
+accounting.mostRecentReport = "Well, well, well...";
+console.log(accounting.mostRecentReport);
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
+
+const someDepartment = new Department("d1", "Accounting");
+someDepartment.addEmployee("Max");
+someDepartment.addEmployee("Manu");
+// someDepartment.employees[2] = "Anna";
+someDepartment.describe();
+someDepartment.printEmployeeInformation();
