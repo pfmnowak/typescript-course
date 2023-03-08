0515a3f918a4b6dff0aec07d77b0038ebdc9c835
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2023-03-08 23:18:35 +0100

    Create a class that implements an interface that extends some other interface

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,1 @@
-interface Person {
-  name: string;
-  age: number;
-
-  greet(phrase: string): void;
-}
-
-let user1: Person;
-
-user1 = {
-  name: "Mikolaj",
-  age: 27,
-  greet(phrase: string) {
-    console.log(phrase + " " + this.name);
-  },
-};
-
-user1.greet("Hello motherfuckers, I'm");
+// Advanced Types
---
+++ src/classes-interfaces/interfaces.ts
@@ -0,0 +1,40 @@
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let addNums: AddFn;
+
+addNums = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name?: string;
+  age = 27;
+
+  constructor(n: string) {
+    if (n) {
+      this.name = n;
+    }
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Mikolaj");
+
+user1.greet("Hello motherfuckers, I'm");
